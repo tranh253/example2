57aef4ff46a0e651c6ef50753512b3b97f4c0ac2
Unknown <trunganh25397@gmail.com>
2017-03-23 09:27:27 +0200

    update customer

---
+++ edit_customer.php
@@ -0,0 +1,25 @@
+<?php include "menu.php";?>
+<h2>Edit Customer</h2>
+<form class="" action="update_all_customers.php" method="post">
+  <table border="0">
+    <tr>
+      <TH>Firstname</TH><TH>Lastname</TH><TH>Streetaddress</TH>
+    </tr>
+
+  <?php
+  include "connection.php";
+  $myquery="SELECT firstname,lastname,streetaddress,id_customers FROM customers";
+  $customers_data=$db->query($myquery);
+
+  foreach ($customers_data as $row) {
+    echo '<tr><td><input type ="text" name="fn[]" value="'.$row['firstname'].'"/></td>';
+    echo '<td><input type ="text" name="fn[]" value="'.$row['lastname'].'"/></td>';
+    echo '<td><input type ="text" name="fn[]" value="'.$row['streetaddress'].'"/></td>';
+    echo '<input type="hidden" name ="id[]" value="'.$row['id_customers'].'"/>';
+    echo '</tr>';
+  }
+  ?>
+  </table>
+  <input type="submit" name="btnUpdateAll" value="Update">
+</form>
+<?php include "footer.php"; ?>
